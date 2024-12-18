12ff1c07bd9518d27cbb2a49676ac1b06726c2bc
Logan <157175146+llogann@users.noreply.github.com>
2024-12-17 16:33:40 -0800

    peg grabbing

--- Jame Gam/Assets/Scripts/PlayerAttach.cs
+++ Jame Gam/Assets/Scripts/PlayerAttach.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerAttach : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CircleCollider2D coll;
+    private bool near = false;
+    [SerializeField] GameObject player;
+
     void Start()
     {
-        
+        coll = GetComponent<CircleCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject == player)
+        {
+            near = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            near = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (near && Input.GetButtonDown("Grab"))
+        {
+            player.transform.parent = transform;
+        }
+        else if (near && Input.GetButtonUp("Grab"))
+        {
+            player.transform.parent = null;
+        }
     }
 }
