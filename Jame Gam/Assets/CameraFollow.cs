3a71f420d9ae9df86a07cc4acebf078f2af038c9
Nicolas Gatien <nicolas.gatien6@gmail.com>
2024-12-18 20:07:29 -0500

    Add Camera Follow

--- Jame Gam/Assets/CameraFollow.cs
+++ Jame Gam/Assets/CameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset = new Vector3(0, 0, -10);
 
     // Start is called before the first frame update
     void Start()
@@ -13,8 +14,9 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 pos = new Vector3(transform.position.x, target.position.y, 0);
+        transform.position = pos + offset;
     }
 }
